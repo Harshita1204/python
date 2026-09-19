e6b4af7f999eeac6af701e401a3ecc462fccbbd7
harshita1204 <harshitak1204@gmail.com>
2026-09-19 11:47:05 +0530

    functions in python

--- pattern.py
+++ pattern.py
@@ -1,8 +1,8 @@
 def pattern():
     for i in range(5):
         for j in range(5):
-            print("*",end=" ")
-        print()
+            print("*",end=" ") # to break the \n we use end = " " to get the output row wise
+        print() # go to the next line we can also use print(end = " ") if we dont use this the output will be in a single row
 pattern()
 pattern()
 pattern()
--- python_function.py
+++ python_function.py
@@ -7,4 +7,20 @@ for i in range(5):
         print("*",end=" ")
     print()
 
-    
+#we are defining the variable inside the function-> locally  that is why there will not be any error of the variables
+# if you want to access the variables then you have to define it somewhere too to access it globally
+def add(a,b):
+    return a+b 
+
+def sub(a,b):
+    return a-b 
+
+def mul(a,b):
+    return a*b  
+
+def calculate(a,b):
+    sum= add(10,20)  
+    diff= sub(10,5)
+    prod= mul(10,20)
+    print(f"sum = {sum} difference = {diff} product = {prod} \n ")
+calculate(10,20)    
